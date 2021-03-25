da2220b41d524165c36a132f8981a381833fbefb
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-03-25 18:16:38 +0700

    ไม่เส็จ

--- Cube Free.c
+++ Cube Free.c
@@ -1,7 +1,8 @@
-#includ(stdio.h)
+#include<stdio.h>
 int n[1000000]={0};
-void cube_free()
-{
+int main()
+{   
+    
 	int i,j,k;
 	for(i=2;i<=100;i++)
 	{
@@ -11,17 +12,13 @@ void cube_free()
 	for(j=k;j<=1000000;j+=k)
 	n[i]=1;
 	}
-}
+    }
 	k=1;
 	for(i=1;i<=1000000;i++)
 	{
 		if(n[i]==0)
 		n[i]=k++;
 	}
-}
-int main()
-{
-	cube_free();
 	int m;
 	scanf("%d",&m);
 	if(n[m]!=1)
---
+++ palindrome.c
@@ -0,0 +1,18 @@
+#includ(stdio.h)
+int main()
+{
+	int count=0;
+	int n,i;
+	scanf("%d",&n);
+	
+	for(i=1;i<=n;i++)
+	{
+		int x=i,sum=0,temp;
+		temp=x;
+		while(x>0)
+		{
+			r=x%10;
+			sum=sum
+		}
+	}
+}
