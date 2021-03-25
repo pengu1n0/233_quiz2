5e38f1f068c3e749002f7a51d6731dfc67c0b027
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-03-25 19:18:50 +0700

    2

---
+++ silk.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int r,t,r1,r2,r3;
+	scanf("%d",&r);
+	scanf("%d",&t);
+	char A[r][70]
+	for(int i=0;i<r;i++)
+	{
+		for(int j=0;j<70;j++)
+		{
+			A[i][j] = 'O';
+		}
+	}
+	for
+}
