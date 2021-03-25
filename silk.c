f0fcaa0e7db4b6db622bd7a6e3d28c84115d0e5b
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-03-25 19:24:22 +0700

    2.1

--- silk.c
+++ silk.c
@@ -1,16 +1,25 @@
 #include<stdio.h>
 int main()
 {
-	int r,t,r1,r2,r3;
+	int r,t,r1,r2,r3,i,j;
 	scanf("%d",&r);
 	scanf("%d",&t);
 	char A[r][70]
-	for(int i=0;i<r;i++)
+	for(i=0;i<r;i++)
 	{
-		for(int j=0;j<70;j++)
+		for( j=0;j<70;j++)
 		{
 			A[i][j] = 'O';
 		}
 	}
-	for
+	for(i=0;i<t;i++)
+	{
+		scanf("%d %d %d",&r1,&r2,&r3);
+		int x=O;
+		for(j=0;j<r3;j++)
+		{
+			A[r1-1][j+2-1]='X';
+		}
+		
+	}
 }
