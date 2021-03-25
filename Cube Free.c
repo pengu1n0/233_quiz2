954317305104988643e3cb54e9971236ffc585bb
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-03-25 17:55:53 +0700

    Update Cube Free.c

--- Cube Free.c
+++ Cube Free.c
@@ -5,9 +5,24 @@ void cude_free
 	int i,j,k;
 	for(i=2;i<=100;i++)
 	{
-		if(n[i]==0)
-	}
+		if(n[i]==0)		
+	{
 	k=i;
 	for(j=k;j<=1000000;j+=k)
 	n[i]=1;
+	}
+}
+	k=1;
+	for(i=1;i<=1000000;i++)
+	{
+		if(n[i]==0)
+		n[i]=k++;
+	}
+}
+int main()
+{
+	cube_free();
+	int m;
+	scanf("%d",&m);
+	
 }
